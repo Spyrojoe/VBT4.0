d616544ec568498171ec566801fdb34f7615b9f6
Brad <brad.brock1@gmail.com>
2015-03-18 23:25:32 -0500

    Revert f82a3a8..975757b
    This rolls back to commit f82a3a858a14b5781fdff8b22f6c4a916c624f8a.

---
+++ js/findTallest.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	var viewHeight = $(window).outerHeight();
+	var colPaddingTop = $(".sidebar").css("padding-top");
+	var compForPadding = viewHeight - colPaddingTop;
+
+	var col1 = $(".sidebar").outerHeight();
+	var col2 = $("main").outerHeight();
+
+});
