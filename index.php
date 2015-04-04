a8e3dbf375d3b9a6323eaee1052ea140dad22b4e
Brad <brad.brock1@gmail.com>
2015-04-03 21:46:08 -0500

    PHP segmenting
    Broke out more content and css

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 <head>
 	<title>Brad's Root Template - v4.0</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" type="text/css" href="min1200.css">
-	<link rel="stylesheet" type="text/css" href="max850.css">
-	<link rel="stylesheet" type="text/css" href="max700.css">
-	<link rel="stylesheet" type="text/css" href="max600.css">
-	<link rel="stylesheet" type="text/css" href="max400.css">
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400|Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+	<?php include("inc/css.php"); ?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 <body>
